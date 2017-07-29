11d2ccd93e37f5d9109c6da3b9788883dba6de4d
Eugene <mentos@i.ua>
2017-07-29 13:37:24 +0300

    Update unitp1.pas

--- unitp1.pas
+++ unitp1.pas
@@ -1,45 +1,39 @@
-unit unitp1;
+unit Unit1;
 
-{$mode objfpc}{$H+}
+{$mode delphi}
 
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, LCLType,
-  Controls, Graphics, Dialogs, StdCtrls, ExtCtrls, Menus, SQLiteTable3;
+  Classes, SysUtils, sqldblib, sqldb, DB, sqlite3conn, FileUtil, Forms,
+  Controls, Graphics, Dialogs, DBGrids, DBCtrls, ExtDlgs, StdCtrls, ExtCtrls,
+  ComCtrls;
 
-const
-{$IF Defined(MSWINDOWS)}
-  SQLiteDLL = 'libsqlcipher.dll';
-{$ELSEIF Defined(DARWIN)}
-  SQLiteDLL = 'libsqlcipher.dylib';
-  {$linklib libsqlite3}
-{$ELSEIF Defined(UNIX)}
-  SQLiteDLL = 'libsqlcipher.so';
-{$IFEND}
+{ TfMain }
 
 type
-
-  { TfrmPersons }
-
-  TfrmPersons = class(TForm)
-    Image1: TImage;
-    logMemo1: TMemo;
-    MainMenu1: TMainMenu;
-    MenuItemAbout: TMenuItem;
-    MenuItemExit: TMenuItem;
-    MenuItemChangePassDb: TMenuItem;
-    MenuItemDelDb: TMenuItem;
-    MenuItemOpenDb: TMenuItem;
-    MenuItemFile: TMenuItem;
-    MenuItemCreateDb: TMenuItem;
+  TfMain = class(TForm)
+    btnLoadPhoto: TButton;
+    btnImportData: TButton;
+    btnSavePhoto: TButton;
+    DataSource1: TDataSource;
+    DBGrid1: TDBGrid;
+    DBImage1: TDBImage;
+    DBMemo1: TDBMemo;
+    DBNavigator1: TDBNavigator;
+    OpenPictureDialog1: TOpenPictureDialog;
+    SavePictureDialog1: TSavePictureDialog;
+    SQLDBLibraryLoader1: TSQLDBLibraryLoader;
+    SQLite3Connection1: TSQLite3Connection;
+    SQLQuery1: TSQLQuery;
+    SQLTransaction1: TSQLTransaction;
+    StatusBar1: TStatusBar;
+    Timer1: TTimer;
+    procedure btnLoadPhotoClick(Sender: TObject);
+    procedure btnSavePhotoClick(Sender: TObject);
     procedure FormClose(Sender: TObject);
     procedure FormCreate(Sender: TObject);
-    procedure MenuItemChangePassDbClick(Sender: TObject);
-    procedure MenuItemCreateDbClick(Sender: TObject);
-    procedure MenuItemDelDbClick(Sender: TObject);
-    procedure MenuItemExitClick(Sender: TObject);
-    procedure MenuItemOpenDbClick(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
   private
     { private declarations }
   public
@@ -47,107 +41,117 @@ type
   end;
 
 var
-  frmPersons: TfrmPersons;
-  pass: string;
-  f1: TFileStream;
-  db: TSQLiteDatabase;
-  sql: string;
-  dbfile: string;
+  fMain: TfMain;
 
 implementation
 
 {$R *.lfm}
 
-{ TfrmPersons }
+{ TfMain }
 
-procedure TfrmPersons.FormCreate(Sender: TObject);
+procedure TfMain.FormCreate(Sender: TObject);
 begin
-  logMemo1.Clear;
-  db := nil;
-  dbfile := GetCurrentDir() + PathDelim + 'persons.db';
-  // Есть файл базы данных ?
-  if not FileExists(dbfile) then
-    if Application.MessageBox('Файл базы данных не найден. Создать пустую базу данных?',
-      'Ошибка', MB_ICONQUESTION + MB_YESNO) = idYes then
-      MenuItemCreateDbClick(self)
-    else
-      Exit();
+  SQLDBLibraryLoader1.LibraryName :=
+    ExtractFilePath(Application.ExeName) + 'sqlite3.dll';
+  SQLDBLibraryLoader1.ConnectionType := 'SQLite3';
+  SQLDBLibraryLoader1.LoadLibrary;
+  SQLDBLibraryLoader1.Enabled := True;
+
+  // указываем путь к базе
+  SQLite3Connection1.DatabaseName :=
+    ExtractFilePath(Application.ExeName) + 'database.db';
+  // указываем рабочую кодировку
+  SQLite3Connection1.CharSet := 'UTF8';
+
+  SQLQuery1.Close;
+  SQLQuery1.SQL.Text := 'select * from "person"';
+
+  try  // пробуем подключится к базе
+    SQLIte3Connection1.Open;
+    SQLTransaction1.Active := True;
+    SQLIte3Connection1.Connected := True;
+  except   // если не удалось то выводим сообщение о ошибке
+    ShowMessage('Ошибка подключения к базе!');
+  end;
+
+  SQLIte3Connection1.ExecuteDirect('PRAGMA key=' + QuotedStr('12345') + ';');
+  SQLTransaction1.Active := True;
+  SQLQuery1.Open;
+
 end;
 
-procedure TfrmPersons.MenuItemChangePassDbClick(Sender: TObject);
+procedure TfMain.Timer1Timer(Sender: TObject);
+begin
+  StatusBar1.Panels[0].Text := TimeToStr(now);
+  StatusBar1.Panels[1].Text := DateToStr(now);
+  StatusBar1.Panels[2].Text := 'Всего записей:'+IntToStr(SQLQuery1.RecNo);
+
+end;
+
+procedure TfMain.FormClose(Sender: TObject);
+begin
+  SQLTransaction1.CloseDataSets;
+  SQLQuery1.Active := False;
+  SQLQuery1.Close;
+  SQLite3Connection1.CloseTransactions;
+  SQLTransaction1.Active := False;
+  SQLite3Connection1.Connected := False;
+  SQLite3Connection1.Close(True);
+
+  SQLTransaction1.Free;
+  SQLite3Connection1.Free;
+
+  SQLDBLibraryLoader1.UnloadLibrary;
+  SQLDBLibraryLoader1.Enabled := False;
+  SQLDBLibraryLoader1.Free;
+end;
+
+procedure TfMain.btnLoadPhotoClick(Sender: TObject);
 var
-  pass1, pass2: string;
+  pictfilename: string;
+  f1: TFileStream;
+  bst: TStream;
 begin
-  pass1 := PasswordBox('Пароль к базе данных', 'Введите новый пароль:');
-  pass2 := PasswordBox('Пароль к базе данных', 'Введите новый пароль еще раз:');
-  if pass1 = pass2 then
-    DB.ModifyDbPassword(PAnsiChar(pass1));
-end;
-
-procedure TfrmPersons.MenuItemCreateDbClick(Sender: TObject);
-begin
-  // Удаляем файл если он есть
-  if FileExists(dbfile) then
-    DeleteFile(dbfile);
-  // Справшиваем пароль
-  // pass := PasswordBox('Пароль к базе данных', 'Введите пароль:');
-  pass := '12345';
-  // Подключение к базе данных, если файл базы данных не существует, будет создана автоматически
-  db := TSQLiteDatabase.Create(dbfile, PAnsiChar(pass));
-  DB.ExecSQL(
-    'CREATE TABLE USER(ID integer PRIMARY KEY AUTOINCREMENT NOT NULL,USERNAME VARCHAR(50),HOMEPAGE VARCHAR(255))');
-  DB.ExecSQL('DROP TABLE IF EXISTS persons;');
-  DB.ExecSQL(
-    'CREATE TABLE persons (id INTEGER PRIMARY KEY AUTOINCREMENT,firstname VARCHAR(30) NOT NULL, middlename VARCHAR (30) NOT NULL, secondname VARCHAR (50) NOT NULL, datebirth  DATE NOT NULL, dateappend DATETIME, prim TEXT);');
-  DB.ExecSQL('DROP TABLE IF EXISTS photo;');
-  DB.ExecSQL(
-    'CREATE TABLE photo (idphoto INTEGER PRIMARY KEY AUTOINCREMENT, photo BLOB NOT NULL, datephoto  DATE, dateappend DATETIME NOT NULL, idperson REFERENCES persons (id));');
-{
+  if OpenPictureDialog1.Execute then
   begin
-    f1 := TFileStream.Create(GetCurrentDir() + PathDelim + '74a4eb79.jpg', fmOpenRead);
-    Sqlite3Dataset1.UpdateBlob('UPDATE photo SET photo = ? WHERE idpersons = ' +
-      IntToStr(id), f1);
-    f1.Free;
+    if SQLQuery1.State <> dsEdit then
+      SQLQuery1.Edit;
+    pictfilename := OpenPictureDialog1.Filename;
+    try
+      f1 := TFileStream.Create(pictfilename, fmOpenRead);
+      try
+        bst := SQLQuery1.CreateBlobStream(SQLQuery1.FieldByName('photo'), bmWrite);
+        bst.CopyFrom(f1, f1.Size);
+      finally
+        bst.Free;
+      end;
+    finally
+      f1.Free;
+    end;
   end;
-  mytable := Sqlite3Dataset1.GetTable('SELECT * FROM persons where id=' +
-    IntToStr(id));
-  ms := TMemoryStream.Create;
-  sql := 'SELECT photo FROM photo WHERE ID=' + IntToStr(id);
-  if Sqlite3Dataset1.GetTable(sql).FieldAsString(0) <> '' then
+end;
+
+procedure TfMain.btnSavePhotoClick(Sender: TObject);
+var
+  JPEGImage: TJPEGImage;
+  ms: TStream;
+begin
+  if not SQLQuery1.FieldByName('photo').IsNull then
   begin
-    ms := Sqlite3Dataset1.GetTable(sql).FieldAsBlob(0);
-    ms.Position := 0;
-    (frmPersons.Image1 as Timage).Picture.Bitmap.LoadFromStream(ms);
-    ms.Free;
+    if SavePictureDialog1.Execute then
+    begin
+      try
+        JPEGImage := TJPEGImage.Create;
+        ms := SQLQuery1.CreateBlobStream(SQLQuery1.FieldByName('photo'), bmRead);
+        ms.Position := 0;
+        JPEGImage.LoadFromStream(ms);
+        JPEGImage.SaveToFile(SavePictureDialog1.Filename);
+      finally
+        JPEGImage.Free;
+        ms.Free;
+      end;
+    end;
   end;
-  }
-end;
-
-procedure TfrmPersons.MenuItemDelDbClick(Sender: TObject);
-begin
-  DB.Free;
-  DeleteFile(dbfile);
-end;
-
-
-procedure TfrmPersons.MenuItemExitClick(Sender: TObject);
-begin
-  Close;
-end;
-
-procedure TfrmPersons.MenuItemOpenDbClick(Sender: TObject);
-begin
-  // pass := PasswordBox('Пароль к базе данных', 'Введите пароль:');
-  pass := '12345';
-  // Подключение к базе данных, если файл базы данных не существует, будет создана автоматически
-  db := TSQLiteDatabase.Create(dbfile, PAnsiChar(pass));
-end;
-
-procedure TfrmPersons.FormClose(Sender: TObject);
-begin
-  DB.Free;
-  // Удаляем файл БД
-  DeleteFile(dbfile);
 end;
 
 end.
